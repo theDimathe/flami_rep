17fbaf65d2be259146bfb94176687d1f354f8c6f
theDimathe <peppmote467@gmail.com>
2026-01-08 02:03:57 +0400

    Add additional quiz steps

---
+++ script.js
@@ -0,0 +1,37 @@
+const screens = Array.from(document.querySelectorAll(".screen"));
+const progressBars = document.querySelectorAll(".progress__bar");
+let currentScreen = 0;
+
+const updateProgress = () => {
+  const screen = screens[currentScreen];
+  const progress = screen.dataset.progress;
+  progressBars.forEach((bar) => {
+    if (!progress) {
+      bar.style.width = "0%";
+    } else {
+      bar.style.width = `${Math.round(Number(progress) * 100)}%`;
+    }
+  });
+};
+
+const showScreen = (index) => {
+  screens[currentScreen].classList.remove("screen--active");
+  currentScreen = Math.max(0, Math.min(index, screens.length - 1));
+  screens[currentScreen].classList.add("screen--active");
+  updateProgress();
+};
+
+document.body.addEventListener("click", (event) => {
+  const nextButton = event.target.closest("[data-next]");
+  const backButton = event.target.closest("[data-back]");
+
+  if (nextButton) {
+    showScreen(currentScreen + 1);
+  }
+
+  if (backButton) {
+    showScreen(currentScreen - 1);
+  }
+});
+
+updateProgress();
